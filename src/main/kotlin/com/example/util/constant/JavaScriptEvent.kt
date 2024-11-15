016b18d6471b92ae876005ac3d91ff0550f77b33
lolmageap <ekxk1234@naver.com>
2024-11-15 17:41:54 +0900

    create constants

---
+++ src/main/kotlin/com/example/util/constant/JavaScriptEvent.kt
@@ -0,0 +1,6 @@
+package com.example.util.constant
+
+object JavaScriptEvent {
+    const val CLICK = "arguments[0].click();"
+    const val REMOVE = "arguments[0].remove();"
+}
---
+++ src/main/kotlin/com/example/util/constant/WebSite.kt
@@ -0,0 +1,10 @@
+package com.example.util.constant
+
+object WebSite {
+    object Yes24 {
+        const val NAME = "yes24"
+        const val URL = ""
+        const val ID = ""
+        const val PASSWORD = ""
+    }
+}
