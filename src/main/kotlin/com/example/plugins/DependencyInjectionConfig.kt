acbd4b29f9ddca796292f626f167f11ee5e49b08
lolmageap <ekxk1234@naver.com>
2024-11-17 23:34:22 +0900

    add dependency injection module

--- src/main/kotlin/com/example/plugins/DependencyInjectionConfig.kt
+++ src/main/kotlin/com/example/plugins/DependencyInjectionConfig.kt
@@ -1,9 +1,10 @@
 package com.example.plugins
 
+import com.example.component.Yes24Browser
+import com.example.usecase.ScrapYes24UseCase
 import io.ktor.server.application.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
-
 
 fun Application.configureDependencyInjection() {
     install(Koin) {
@@ -12,5 +13,6 @@ fun Application.configureDependencyInjection() {
 }
 
 val module = module {
-
+    single<ScrapYes24UseCase> { ScrapYes24UseCase(get()) }
+    single<Yes24Browser> { Yes24Browser() }
 }
