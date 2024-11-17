4f99505798a3f35f6031c7ea55706f2441f6a6a7
lolmageap <ekxk1234@naver.com>
2024-11-17 23:37:19 +0900

    refactor to driver functions

--- src/main/kotlin/com/example/util/extension/ChromeDriver.kt
+++ src/main/kotlin/com/example/util/extension/ChromeDriver.kt
@@ -7,18 +7,13 @@ import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import org.openqa.selenium.By
 import org.openqa.selenium.JavascriptExecutor
-import org.openqa.selenium.OutputType
-import org.openqa.selenium.TakesScreenshot
-import org.openqa.selenium.UnhandledAlertException
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
-import java.io.File
-import java.time.Duration
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit.MILLIS
+import kotlin.time.Duration
+import kotlin.time.toJavaDuration
 
 private val logger = KotlinLogging.logger { }
 
@@ -40,37 +35,6 @@ suspend fun ChromeDriver.access(
         }
     }
 
-suspend fun <T> ChromeDriver.switchToIframe(
-    iframeId: String = "Contents",
-    duration: Duration = Duration.of(1, MILLIS),
-    block: suspend RemoteWebDriver.() -> T,
-): T {
-    return withContext(IO) {
-        manage().timeouts().implicitlyWait(duration)
-        switchTo().frame(iframeId)
-
-        try {
-            block().apply {
-                runCatching {
-                    switchTo().alert().dismiss()
-                }
-                switchTo().defaultContent()
-            }
-        } catch (e: UnhandledAlertException) {
-            runCatching {
-                switchTo().alert().dismiss()
-            }.onFailure {
-                logger.error { "알림창이 없어요" }
-            }
-            switchToIframe(iframeId, duration, block)
-        } catch (e: Exception) {
-            exit()
-            logger.error(e) { "iframe 작업 중 문제가 발생했어요" }
-            throw e
-        }
-    }
-}
-
 suspend fun RemoteWebDriver.hasElement(
     by: By,
 ) =
@@ -85,38 +49,34 @@ suspend fun RemoteWebDriver.hasElement(
 
 suspend fun RemoteWebDriver.waitUntilElementRendered(
     element: By,
-    waitTimeSecond: Int = 5,
+    waitTime: Duration,
 ) {
-    val waitTime = Duration.ofSeconds(waitTimeSecond.toLong())
     withContext(IO) {
-        val wait = WebDriverWait(this@waitUntilElementRendered, waitTime)
+        val wait = WebDriverWait(this@waitUntilElementRendered, waitTime.toJavaDuration())
         wait.until(ExpectedConditions.visibilityOfElementLocated(element))
     }
 }
 
 suspend fun RemoteWebDriver.isElementRendered(
     element: By,
-    waitTimeSecond: Int = 5,
-): Boolean {
-    val waitTime = Duration.ofSeconds(waitTimeSecond.toLong())
-    return withContext(IO) {
+    waitTime: Duration,
+) =
+    withContext(IO) {
         try {
-            val wait = WebDriverWait(this@isElementRendered, waitTime)
+            val wait = WebDriverWait(this@isElementRendered, waitTime.toJavaDuration())
             wait.until(ExpectedConditions.visibilityOfElementLocated(element))
             true
         } catch (e: Exception) {
             false
         }
     }
-}
 
 suspend fun RemoteWebDriver.waitUntilElementRemoved(
     element: By,
-    waitTimeSecond: Int = 5,
+    waitTime: Duration,
 ) {
-    val waitTime = Duration.ofSeconds(waitTimeSecond.toLong())
     withContext(IO) {
-        val wait = WebDriverWait(this@waitUntilElementRemoved, waitTime)
+        val wait = WebDriverWait(this@waitUntilElementRemoved, waitTime.toJavaDuration())
         wait.until { driver ->
             val elements = driver.findElements(element)
             elements.isEmpty() || elements.all { !it.isDisplayed }
@@ -131,14 +91,6 @@ suspend fun ChromeDriver.doubleClickToJavaScript(
         toJavascriptExecutor().executeScript(CLICK, button)
         toJavascriptExecutor().executeScript(CLICK, button)
     }
-}
-
-fun ChromeDriver.screeShot() {
-    val screenshot = this as TakesScreenshot
-    val srcFile = screenshot.getScreenshotAs(OutputType.FILE)
-    val destFile = File("screenshots/${LocalDateTime.now()}.png")
-    srcFile.copyTo(destFile)
-    logger.error { "스크린샷 -> screenshots/${LocalDateTime.now()}.png" }
 }
 
 fun RemoteWebDriver.toJavascriptExecutor() =
