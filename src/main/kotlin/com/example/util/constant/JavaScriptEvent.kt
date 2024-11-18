58def5968ca5a56280187821a44b0683812e0a25
lolmageap <ekxk1234@naver.com>
2024-11-18 20:32:50 +0900

    add web constant

--- src/main/kotlin/com/example/util/constant/JavaScriptEvent.kt
+++ src/main/kotlin/com/example/util/constant/JavaScriptEvent.kt
@@ -3,4 +3,5 @@ package com.example.util.constant
 object JavaScriptEvent {
     const val CLICK = "arguments[0].click();"
     const val REMOVE = "arguments[0].remove();"
+    const val REDIRECT = "window.location.href = arguments[0];"
 }
--- src/main/kotlin/com/example/util/constant/WebSite.kt
+++ src/main/kotlin/com/example/util/constant/WebSite.kt
@@ -3,7 +3,7 @@ package com.example.util.constant
 object WebSite {
     object Yes24 {
         const val NAME = "yes24"
-        const val URL = ""
+        const val URL = "https://www.yes24.com/Templates/FTLogin.aspx?ReturnURL=http://ticket.yes24.com/Pages/MyPage/MyPageMain.aspx"
         const val ID = ""
         const val PASSWORD = ""
     }
