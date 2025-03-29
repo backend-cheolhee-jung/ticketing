277dfa85b3e4fded302e048020811431cdeeb6a4
lolmageap <ekxk1234@naver.com>
2025-03-29 22:42:25 +0900

    remove unused express dependency

--- chrome-extension/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response, NextFunction } from "express";
-
-const router = Router();
-
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.render("index", { title: "Express" });
-});
-
-export default router;
--- chrome-extension/src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response, NextFunction } from "express";
-
-const router = Router();
-
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Users page");
-});
-
-export default router;
