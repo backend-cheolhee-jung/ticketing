fe711798c3d29372283f8753a0e986ef288e4340
lolmageap <ekxk1234@naver.com>
2025-04-12 20:32:23 +0900

    implement email notification on image capture

--- desktop/src/main/kotlin/AutoCapture.kt
+++ desktop/src/main/kotlin/AutoCapture.kt
@@ -1,5 +1,8 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import org.apache.commons.mail.SimpleEmail
+
+private val mailSender = ApacheSimpleMailSender(SimpleEmail())
 
 @Composable
 fun autoCapture(
@@ -19,7 +22,13 @@ fun autoCapture(
         else {
             oldImage.deleteImage()
             newImage.saveImage()
-//            send mail
+            mailSender.send(
+                Mail(
+                    to = "ekxk1234@gmail.com",
+                    subject = "이미지 바꼈습니다.",
+                    body = "이미지 바꼈습니다.",
+                )
+            )
         }
     }
 }
