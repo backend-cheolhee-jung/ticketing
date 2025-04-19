24e0deffc6b2ff3d416d2dff59f5c4756b63b14c
lolmageap <ekxk1234@naver.com>
2025-04-20 00:20:37 +0900

    enhance website table schema with validation checks for fields

--- desktop/src/main/kotlin/Website.kt
+++ desktop/src/main/kotlin/Website.kt
@@ -1,16 +1,17 @@
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.trim
 
-object Websites: Table("website_urls") {
+object Websites : Table("website_urls") {
     val id = long("id").autoIncrement()
-    val name = varchar("name", 255)
-    val url = varchar("url", 255).uniqueIndex("url_unique")
-    val loginUrl = varchar("login_url", 255)
-    val email = varchar("email", 255)
-    val password = varchar("password", 255)
-    val idInput = varchar("id_input", 255)
-    val passwordInput = varchar("password_input", 255)
-    val loginButtonElement = varchar("login_button_element", 255)
+    val name = varchar("name", 255).uniqueIndex().check { it.trim() neq "" }
+    val url = varchar("url", 255).uniqueIndex("url_unique").check { it.trim() neq "" }
+    val loginUrl = varchar("login_url", 255).check { it.trim() neq "" }
+    val email = varchar("email", 255).check { it.trim() neq "" }.check { it like "%@%" }
+    val password = varchar("password", 255).check { it.trim() neq "" }
+    val idInput = varchar("id_input", 255).check { it.trim() neq "" }
+    val passwordInput = varchar("password_input", 255).check { it.trim() neq "" }
+    val loginButtonElement = varchar("login_button_element", 255).check { it.trim() neq "" }
 
     override val primaryKey = PrimaryKey(id)
 }
