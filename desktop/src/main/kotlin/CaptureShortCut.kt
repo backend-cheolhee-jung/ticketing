1446b82f5eb180ed976c69e895cc76c0a3d013c0
lolmageap <ekxk1234@naver.com>
2025-05-05 23:27:16 +0900

    add focus requester

--- desktop/src/main/kotlin/CaptureShortCut.kt
+++ desktop/src/main/kotlin/CaptureShortCut.kt
@@ -4,14 +4,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.key.*
 
 @Composable
 fun captureShortcut() {
     var isCaptureMode by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .focusRequester(focusRequester)
             .onPreviewKeyEvent { event ->
                 val isKeyDown = event.type == KeyEventType.KeyDown
                 val isCKeyword = event.key == Key.C
